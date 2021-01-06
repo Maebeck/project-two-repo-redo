7ca5ab6587c20d547e8cca323acbaf3f8110b718
Corey Smith <CSSmith1997@gmail.com>
2021-01-05 23:56:15 -0700

    Created routes in home-routes for main.handlebars so you can navigate through the different pages.

--- src/server/controllers/home-routes.js
+++ src/server/controllers/home-routes.js
@@ -24,4 +24,26 @@ router.get('/private', async (req, res) => {
       res.status(400).json(err);
     }
   });
+  router.get('/home', async (req, res) => {
+    try {
+      res.render('home');
+    } catch (err){
+      res.status(500).json(err);
+    }
+  });
+  router.get('/login', async (req, res) => {
+    try {
+      res.render('login');
+    } catch (err){
+      res.status(500).json(err);
+    }
+  });
+  router.get('/contact', async (req, res) => {
+    try {
+      res.render('contact');
+    } catch (err){
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
